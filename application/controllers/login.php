52dc8646d354bcb6771abe9e9be6ee27a66c3a4f
linauror <linauror@gmail.com>
2013-04-26 19:39:02 +0800

    增加前台登录注册控制器

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -25,13 +25,12 @@ class Login extends CI_Controller
         $this->load->model('Comment_mdl');
         
         $this->load->vars(array('current_nav' => 'index'));
-        $categorys= $this->Category_mdl->get_list();
         $html['siteconfig'] = $this->Siteconfig_mdl->get_list(array('varname' => 'sitename,keyword,description', 'select' => 'varname,value'), true);
-        $html['categorys'] = $categorys;
         $html['post_hot'] = $this->Post_mdl->get_list(array('select' => 'category,title,slug,click,posttime', 'ishidden' => 0, 'limit' => 10, 'posttime' => true, 'orderby' => 'click DESC', 'onlylist' => true));
         $html['comments_new'] = $this->Comment_mdl->get_list(array('ishidden' => 0, 'ispass' => 1, 'limit' => 10, 'onlylist' => true, 'onlylist' => true));
         $html['friendlink'] = $this->Friendlink_mdl->get_list(array('ishidden' => 0));
         $this->load->view('login', $html);
+        $this->output->cache(config_item('cache_time'));
     }
 
     // ------------------------------------------------------------------------
@@ -62,6 +61,45 @@ class Login extends CI_Controller
             show_message('请填写完整！', 'login?refer='.$refer, '错误提示');
         }
     }
+    
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Login::register()
+     * 注册
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->User_mdl->uid) redirect('profile');
+        $post = $this->input->post();
+        if ($post) {
+            $error = array(-1 => '账号长度不符', -2 => '账号格式不正确', -3 => '邮箱格式不正确', -4 => '密码长度不符', -5 => '用户名已经存在', -6 => '用户邮箱已经存在');
+            $user['username'] = $post['username'];
+            $user['password'] = $post['password'];
+            $user['usermail'] = $post['usermail'];
+            $user['userurl'] = $post['userurl'];
+            $user['group'] = 3;
+            $register = $this->User_mdl->add($user);
+            if ($register < 0) {
+                show_error($error[$register]);
+            }
+            show_message('恭喜你，注册成功，请登录', 'login');
+        }
+        
+        $this->load->model('Friendlink_mdl');
+        $this->load->model('Post_mdl');
+        $this->load->model('Comment_mdl');
+        
+        $this->load->vars(array('current_nav' => 'index'));
+        $html['siteconfig'] = $this->Siteconfig_mdl->get_list(array('varname' => 'sitename,keyword,description', 'select' => 'varname,value'), true);
+        $html['post_hot'] = $this->Post_mdl->get_list(array('select' => 'category,title,slug,click,posttime', 'ishidden' => 0, 'limit' => 10, 'posttime' => true, 'orderby' => 'click DESC', 'onlylist' => true));
+        $html['comments_new'] = $this->Comment_mdl->get_list(array('ishidden' => 0, 'ispass' => 1, 'limit' => 10, 'onlylist' => true, 'onlylist' => true));
+        $html['friendlink'] = $this->Friendlink_mdl->get_list(array('ishidden' => 0));
+        $this->load->view('register', $html);
+        $this->output->cache(config_item('cache_time'));
+    }    
 
     // ------------------------------------------------------------------------
 
