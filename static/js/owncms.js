e99f6d6c1cd3e82440ee437011d5a7e54e5b6345
linauror <linauror@gmail.com>
2013-05-04 19:43:47 +0800

    图片预览脚本

--- static/js/owncms.js
+++ static/js/owncms.js
@@ -1,4 +1,19 @@
 $(function(){
+    
+    $('.post_list .post_content img').click(function(){
+        $('.preview_img').remove();
+        var tmpimg = new Image();
+        tmpimg.src = $(this).attr('src');
+        ow = tmpimg.width;
+        oh = tmpimg.height;
+        $('body').append('<img src = "' + $(this).attr('src') + '" class= "preview_img" title="点击关闭" />');
+        $('.preview_img').height(oh).width(ow).css({ marginLeft: '-' + ow / 2 + 'px', marginTop : parseInt('-' + oh / 2) + $(document).scrollTop() + 'px'}).show('normal');
+    })
+    
+    $('.preview_img').live('click', function(){
+        $(this).hide('normal');
+    })
+    
     $.post(base_url+'uapi/checklogin', {uid : uid}, function(data){
         if (data) {
             html = '';
