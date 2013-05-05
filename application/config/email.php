aa2e046058d6d06271163cc5519f9ae1c6aa7180
linauror <linauror@gmail.com>
2013-05-06 00:18:17 +0800

    邮件配置文件

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['protocol'] = 'smtp';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['smtp_host'] = 'smtp.exmail.qq.com';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_port'] = '25';
+
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
