891961946301465053b0d3030fd37467cc28003b
linauror <linauror@gmail.com>
2013-05-16 23:37:45 +0800

    改用helper函数获取菜单

--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,4 @@
 <div id="header">
 	<h1 class="sitename"><a href="<?php echo base_url();?>" title="<?php echo $siteconfig['sitename'];?>"><?php echo $siteconfig['sitename'];?></a></h1>
-    <?php echo $this->Menu_mdl->show_menu_nav(0, $current_nav, 'nav_main');?>
+    <?php echo show_menu_nav($this->Menu_mdl->get_list(), 0, $current_nav, 'nav_main');?>
 </div>
