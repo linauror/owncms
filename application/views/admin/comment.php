8aec551af32007aa3d60e57673182b219551d16d
linauror <linauror@gmail.com>
2013-05-17 09:45:33 +0800

    评论后台显示IP

--- application/views/admin/comment.php
+++ application/views/admin/comment.php
@@ -24,7 +24,7 @@
         <tr>
           <td><input type="checkbox" name="id[]" class="checkbox" value="<?php echo $line['id'];?>" /></td>
           <td><img src="http://www.gravatar.com/avatar/<?php echo md5(strtolower($line['usermail']));?>?s=40" /></td>
-          <td><?php echo $line['username']?>[<?php echo $line['uid'] ? '<a href="?username='.$line['username'].'">'.$line['username'].'</a>' : '未注册';?>]<br /><a href="<?php echo $line['userurl']?>" target="_blank"><?php echo $line['userurl']?></a></td>
+          <td><?php echo $line['username']?>[<?php echo $line['uid'] ? '<a href="?username='.$line['username'].'">'.$line['username'].'</a>' : '未注册';?>]<br /><a href="<?php echo $line['userurl']?>" target="_blank"><?php echo $line['userurl']?></a> <?php echo $line['ip']?></td>
           <td>提交于 <?php echo $line['posttime'];?><br /><?php echo $line['content'];?></td>
           <td><a href="?pid=<?php echo $line['pid'];?>"><?php echo $line['title'];?></a></td>
 		  <td><a href="<?php echo site_url('admin/comment/del/'.$line['id']);?>" title="删除评论">删除</a> | <a href="<?php echo site_url('admin/post/view/'.$line['pid']);?>" target="_blank" title="查看文章">查看文章</a> | <a href="<?php echo site_url('admin/comment/edit/'.$line['id'].'?ishidden='.$line['ishidden']);?>">设为[<?php echo $line['ishidden'] ? '显示' : '隐藏' ?>]</a> | <a href="<?php echo site_url('admin/comment/edit/'.$line['id'].'?ispass='.$line['ispass']);?>">设为[<?php echo $line['ispass'] ? '通过' : '审核' ?>]</a></td>
